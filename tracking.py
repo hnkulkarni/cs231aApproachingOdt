62c082335b366298018da9fa39f65202b7306247
Hrushikesh Kulkarni <hnkulkarni@gmail.com>
2021-03-19 08:04:58 -0700

    Adding plots

--- tracking.py
+++ tracking.py
@@ -13,7 +13,7 @@ def match_detections(prev_path, prev_detection, new_path, new_detection, size=(6
 
     permutations = myutils.unique_permutations(prev_range, new_range)
 
-    f, ax = plt.subplots(1, 2)
+    fig, ax = plt.subplots(1, 2)
     prev_img = myutils.load_resize(prev_path, size)
     new_img = myutils.load_resize(new_path, size)
 
@@ -21,10 +21,12 @@ def match_detections(prev_path, prev_detection, new_path, new_detection, size=(6
         [a.cla() for a in ax]
         draw_detection(prev_img, prev_detection[old], ax[0])
         ax[0].set_title(f"{os.path.basename(prev_path)}")
+
         draw_detection(new_img, new_detection[new], ax[1])
         ax[1].set_title(f"{os.path.basename(new_path)}")
         plt.pause(0.1)
 
+    plt.close(fig)
 
 def draw_detection(img, detection, ax):
     ax.imshow(myutils.bgr2rgb(img))
@@ -45,9 +47,7 @@ def tracking_by_detection(image_paths, img_detections, size=(640, 480)):
 
     for img_i, (path, detections) in enumerate(tqdm_pbar):
         tqdm_pbar.set_postfix({"Processing ": path})
-        print(path)
         if img_i == 0:
-
             print("Initialize Detections")
             continue
 
