91774f81a02c04ed403813ea5de84dfb1ec4240f
Hrushikesh Kulkarni <hnkulkarni@gmail.com>
2021-03-18 22:56:50 -0700

    Refactor

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 ## This is the code for the CS231A class project to detect approaching objects
 import argparse
-
-import pandas as pd
-import tqdm
-import numpy as np
-
 import ObjDetector
 
 def main():
-    print(f"The main of the project")
     parser = argparse.ArgumentParser()
     parser.add_argument("--image_folder", type=str, default="data/samples", help="path to dataset")
 
@@ -16,8 +10,6 @@ def main():
     print(opt)
 
     imgs, img_detections = ObjDetector.detections(image_folder=opt.image_folder, batch_size=24)
-    print(imgs)
-    print(img_detections)
 
 
 if __name__ == '__main__':
