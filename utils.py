9d37fd0dc90afa2cdd2725c399df7394be223920
Hrushikesh Kulkarni <hnkulkarni@gmail.com>
2021-03-19 07:03:02 -0700

    Adding utils

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import glob
+import itertools
 import os
-import natsort
+
 import cv2
+import natsort
 
 
 def files_from_folder(folderpath, ext):
@@ -9,11 +11,19 @@ def files_from_folder(folderpath, ext):
     files = natsort.natsorted(files)
     return files
 
+
 def load(f):
     img = cv2.imread(f)
     return img
+
 
 def show(img):
     cv2.imshow('image', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+def unique_permutations(list1, list2):
+    # Source From: https://stackoverflow.com/questions/12935194/permutations-between-two-lists-of-unequal-length
+    result = [list(zip(x, list2)) for x in itertools.permutations(list1, len(list2))]
+    return result
