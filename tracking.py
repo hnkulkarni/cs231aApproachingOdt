85cad14eef585d2d7cf347c2dc0906d98b502567
Hrushikesh Kulkarni <hnkulkarni@gmail.com>
2021-03-19 15:18:50 -0700

    Adding pickle data

--- tracking.py
+++ tracking.py
@@ -29,7 +29,7 @@ def match_detections(prev_path, prev_detection, new_path, new_detection, size=(6
 
         draw_detection(new_img, new_detection[new], ax[1])
         ax[1].set_title(f"{os.path.basename(new_path)}")
-        plt.pause(0.1)
+        #plt.pause(0.1)
         iou = get_iou(prev_detection[old], new_detection[new])
 
         if iou < 0.7:
@@ -41,7 +41,7 @@ def match_detections(prev_path, prev_detection, new_path, new_detection, size=(6
         methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
                    'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
 
-        is_match = template_matching(prev_crop, new_crop, methods[3])
+        is_match = template_matching(new_crop, prev_crop, methods[3])
 
         if is_match == True:
             matching_pairs.append((old, new))
@@ -57,34 +57,41 @@ def get_iou(prev_detection, new_detection):
 
 def template_matching(img1, template, method):
     fig_template, ax = plt.subplots()
+
     template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
     img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     img = img1_gray.copy()
-    w, h = template_gray.shape[::-1]
+    w_t, h_t = template_gray.shape[::-1]
+    w_i, h_i = img1_gray.shape[::-1]
+
+    if (w_t > w_i) or (h_t > h_i):
+        return False
 
     method = eval(method)
     # Apply template Matching
     res = cv2.matchTemplate(img1_gray, template_gray, method)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-    print(f"\n{min_val}, {max_val}, {min_loc}, {max_loc}")
+    #print(f"\n{min_val}, {max_val}, {min_loc}, {max_loc}")
     # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
     if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
         top_left = min_loc
     else:
         top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
-    cv2.rectangle(img, top_left, bottom_right, 255, 2)
+    # bottom_right = (top_left[0] + w, top_left[1] + h)
+    # cv2.rectangle(img, top_left, bottom_right, 255, 2)
 
-    plt.subplot(121), plt.imshow(res, cmap='gray')
-    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122), plt.imshow(img, cmap='gray')
-    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-    plt.suptitle(method)
-    plt.show()
-    plt.close(fig_template)
+    # plt.subplot(121), plt.imshow(res, cmap='gray')
+    # plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+    # plt.subplot(122), plt.imshow(img, cmap='gray')
+    # plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+    # plt.suptitle(method)
+    # plt.show()
+    # plt.close(fig_template)
 
     if max_val > 0.9:
         return True
+    else:
+        return False
 
 def keypoint_matching(img1, img2):
     # Source: https://docs.opencv.org/master/dc/dc3/tutorial_py_matcher.html
@@ -134,6 +141,7 @@ def tracking_by_detection(image_paths, img_detections, size=(640, 480)):
     num_images = len(image_paths)
     tqdm_pbar = tqdm.tqdm(path_detections_zip, total=num_images)
 
+    tracks_dict = dict()
     for img_i, (path, detections) in enumerate(tqdm_pbar):
         tqdm_pbar.set_postfix({"Processing ": path})
         if img_i == 0:
@@ -143,3 +151,7 @@ def tracking_by_detection(image_paths, img_detections, size=(640, 480)):
         matching_pairs = match_detections(prev_path=image_paths[img_i - 1], prev_detection=img_detections[img_i - 1],
                          new_path=path, new_detection=detections, size=size)
         print(matching_pairs)
+        tracks_dict[path] = matching_pairs
+
+    myutils.pickle_save("tracks.pickle", tracks_dict)
+    return tracks_dict
