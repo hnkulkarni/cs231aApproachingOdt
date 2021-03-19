90cfe23c7f207a6eed9719327e095138b21579f1
Hrushikesh Kulkarni <hnkulkarni@gmail.com>
2021-03-18 22:43:39 -0700

    Getting Yolo detections

--- ObjDetector.py
+++ ObjDetector.py
@@ -3,6 +3,7 @@ import time
 import torch
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
+from torch.autograd import Variable
 from PIL import Image
 import os
 import time
@@ -11,15 +12,18 @@ import datetime
 from PyTorchYOLOv3.models import Darknet
 from PyTorchYOLOv3.utils.datasets import ImageFolder
 from PyTorchYOLOv3.utils.augmentations import DEFAULT_TRANSFORMS, Resize
-from PyTorchYOLOv3.utils.utils import load_classes
+from PyTorchYOLOv3.utils.utils import load_classes, non_max_suppression
 
 def detections(image_folder, batch_size):
     print("Getting Yolo Detections")
-    model_def = "/home/hnkulkarni/nn/opensource/PyTorchYOLOv3/config/yolov3.cfg"
-    weights_path = "/PyTorchYOLOv3/weights/yolov3.weights"
-    class_path = "/PyTorchYOLOv3/data/coco.names"
+    YOLO_HOME = "/home/hnkulkarni/nn/opensource/PyTorchYOLOv3"
+    model_def = os.path.join(YOLO_HOME, "config/yolov3.cfg")
+    weights_path = os.path.join(YOLO_HOME,"weights/yolov3.weights")
+    class_path = os.path.join(YOLO_HOME,"data/coco.names")
     nms_thres = 0.4
     img_size = 416
+    conf_thres = 0.8
+
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -47,7 +51,21 @@ def detections(image_folder, batch_size):
     img_detections = []  # Stores detections for each image index
 
     print("\nPerforming object detection:")
-
-    print("\nPerforming object detection:")
     prev_time = time.time()
+
+    for batch_i, (img_paths, input_imgs) in enumerate(dataloader):
+        # Configure Input
+        input_imgs = Variable(input_imgs.type(Tensor))
+
+        # get detections
+        with torch.no_grad():
+            detections = model(input_imgs)
+            detections = non_max_suppression(detections, conf_thres=conf_thres, nms_thres=nms_thres)
+
+        current_time = time.time()
+        inference_time = datetime.timedelta(seconds=current_time - prev_time)
+        prev_time = current_time
+        print(f" {batch_i} : Inference Time =  {inference_time}")
+    return imgs, img_detections
+
 
